41e3ffc903697df3e243d03761aa8ab1542f2d77
Evening Star <shjoseph75@gmail.com>
2026-05-03 23:16:58 +0300

    use postgres database

--- shop/settings.py
+++ shop/settings.py
@@ -73,11 +73,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        'postgresql://stationery_lzme_user:6G2JS88yLMO6zOYzpAb9UOSzArmSxaJf@dpg-d7rq4fdckfvc73a8eg20-a.oregon-postgres.render.com/stationery_lzme'
+    )
 }
 
 
