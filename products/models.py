035f2a69be7092e2ae8863c578c1a14193cb72f9
Evening Star <shjoseph75@gmail.com>
2026-05-07 02:45:54 +0300

    add cost price

--- products/models.py
+++ products/models.py
@@ -4,6 +4,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
+    cost_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
 
     def __str__(self):
         return self.name
@@ -15,7 +17,7 @@ class Sale(models.Model):
     quantity_sold = models.IntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return f"{self.product.name} - {self.quantity_sold}"
     
